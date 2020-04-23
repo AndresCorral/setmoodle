7b93d55e48835564a1376cf146076523c0f7d40f
Andrés Corral <33558356+AndresCorral@users.noreply.github.com>
2020-04-23 14:48:12 -0500

    Update moodle37.sh
    se agrega descarga de thema moove

--- moodle37.sh
+++ moodle37.sh
@@ -6,4 +6,8 @@ wget https://download.moodle.org/download.php/direct/stable37/moodle-latest-37.t
 sudo tar xf moodle-latest-37.tgz
 sudo mv /var/www/html/moodle/* /var/www/html/
 sudo rm -R /var/www/html/moodle
-
+sudo apt-get install unzip
+sudo chmod 777 /var/www/html/theme
+cd theme
+wget https://moodle.org/plugins/download.php/19591/theme_moove_moodle37_2019043000.zip
+sudo unzip theme_moove_moodle37_2019043000.zip
