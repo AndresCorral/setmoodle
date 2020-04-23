41c3a3887f25b938627781cdd5c57a28af61a720
Andrés Corral <33558356+AndresCorral@users.noreply.github.com>
2020-04-23 14:00:19 -0500

    Update moodle37.sh

--- moodle37.sh
+++ moodle37.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
+sudo chmod 777 /var/www/html
 cd /var/www/html
-sudo mkdir moodle37
-sudo chmod 777 /var/www/html/moodle37
-cd moodle37
 wget https://download.moodle.org/download.php/direct/stable37/moodle-latest-37.tgz
 sudo tar xf moodle-latest-37.tgz
-sudo mv /var/www/html/moodle37/moodle/* /var/www/html/moodle37
-sudo rm -R /var/www/html/moodle37/moodle
+sudo mv /var/www/html/moodle/* /var/www/html/
+sudo rm -R /var/www/html/moodle
 
